e213a89fabcd004caf9f8b61eef086f0d323439b
Omar Elsendiony <omar.sendiony@gmail.com>
2023-10-31 20:40:31 +0200

    g

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+def fizzbuzz():
+    for i in range(100):
+        if (i % 3 and i % 5):
+            print("fizzBuzz", end = " ")
+        elif (i % 3):
+            print("Fizz", end = " ")
+        elif (i % 5):
+            print("Buzz", end = " ")
+        else:
+            print(number + " ")
