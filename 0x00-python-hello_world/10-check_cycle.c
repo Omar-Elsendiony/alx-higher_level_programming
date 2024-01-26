5b03ff79e2a697d2b2c8b9f772bce123e1c0dea5
Omar Elsendiony <omar.sendiony@gmail.com>
2024-01-26 10:30:51 +0200

    see

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,57 +1,26 @@
 #include "lists.h"
 
-
-int traverseLists(listint_t **storage, listint_t *checker)
-{
-    /* the first element is the number*/
-    int number;
-    listint_t * current;
-    current = *storage;
-    while (current != NULL)
-    {
-        number = (current)->n;
-        if (number == checker->n)
-        {
-            return 1;
-        }
-        current = current->next;
-    }
-    return 0;
-}
-
+/**
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
+ */
 int check_cycle(listint_t *list)
 {
-    /* add only one element temporarily*/
-    int res;
-    listint_t *storage = malloc(sizeof(listint_t)), *current;
-    listint_t *tempStorage;
-    listint_t *HEAD;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-    current = list;
-    if (current != NULL)
-    {
-        storage->n = current->n;
-        storage->next = NULL;
-    }
-    else
-    {
-        return 0;
-    }
-    current = current->next;
-    HEAD = storage;
-    while (current != NULL)
-    {
-        res = traverseLists(&HEAD, current);
-        if (res == 1)
-            return 1;
-        
-        tempStorage = malloc(sizeof(listint_t));
-        tempStorage->n = current->n;
-        tempStorage->next = NULL;
-        storage->next = tempStorage;
-        storage = tempStorage;
-        current = current->next;
-    }
-    return 0;
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
 }
-
